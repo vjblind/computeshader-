e8e33d9e7224797ef0eddae169ab3985c2042814
vjblind <vjblind@users.noreply.github.com>
2017-12-26 17:47:38 +0100

    computeshader
    computeshader

---
+++ compute.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class compute : MonoBehaviour {
+    /*[DllImport("dllproject", EntryPoint = "TestSort")]
+    public static extern void TestSort(int[] a, int length);*/
+
+    public Material material;
+    public ComputeShader computeShader;
+    ComputeBuffer buffer;
+    public int CSMainIndex;
+    public int count = 5000000;
+    const float size = 5.0f;
+   private const int  c_groupesize=128;
+    private const int c_stride = 12;
+    struct Vert
+    {
+      public Vector3 pos;
+    };
+
+ 
+  
+ 
+    // Use this for initialization
+    void Start()
+
+    {
+
+        // buffer=new ComputeBuffer(count,sizeof(float)*3,ComputeBufferType.Default);
+
+        //  CSMainIndex = computeShader.FindKernel("CSMain");
+
+        buffer = new ComputeBuffer(count, sizeof(float)*4);
+
+
+        Vert[] points = new Vert[count];
+        for (int i = 0; i < count; i++)
+
+        {
+               points[i].pos   =  Random.insideUnitSphere*3;
+   
+        }
+        Debug.Log("" + points[3].pos);
+        Debug.Log("" + points[4].pos);
+        buffer.SetData(points);
+
+    }
+
+    void OnRenderObject()
+    {  material.SetPass(0);
+       material.SetBuffer("buffer", buffer);
+         Graphics.DrawProcedural(MeshTopology.Points, count, 1);
+    }
+   
+    private void OnDestroy()
+    {
+        if (buffer != null)
+            buffer.Release();
+         
+    }
+    // Update is called once per frame
+    void Update () {
+       computeShader.SetBuffer(computeShader.FindKernel("CSMain"), "buffer", buffer);
+        //  computeShader.SetFloat(("deltaTime"),Time.deltaTime);
+        int nbgroupes = Mathf.CeilToInt((float)count / c_groupesize);
+       computeShader.Dispatch(computeShader.FindKernel("CSMain"), nbgroupes, 1, 1);
+       
+    }
+}
---
+++ shader.shader
@@ -0,0 +1,91 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+  Shader "Custom/Shader"
+
+	{
+
+		SubShader
+
+		{
+
+			Pass
+
+		{
+
+			ZTest Always Cull Off ZWrite Off
+
+			Fog{ Mode off }
+
+
+
+			CGPROGRAM
+
+#include "UnityCG.cginc"
+
+#pragma target 5.0
+
+#pragma vertex vert
+
+#pragma fragment frag
+
+ 
+
+
+			struct Vert
+		{
+			  float3 pos;
+		};
+
+
+
+
+
+		uniform StructuredBuffer<Vert> buffer;
+
+	 
+		struct v2f
+
+		{
+
+			float4  pos : SV_POSITION;
+
+		 
+
+		};
+		float3  h=0;
+
+
+		v2f vert(uint id : SV_VertexID)
+
+		{
+
+			float3 pos = buffer[id].pos;
+			h = pos;
+			v2f OUT;
+
+			OUT.pos = UnityObjectToClipPos(float4(buffer[id].pos, 1));
+			
+			h = OUT.pos;
+			return OUT;
+
+		}
+
+
+
+		float4 frag(v2f IN) : COLOR
+
+		{
+
+			return float4(1,0,0,1);
+
+		}
+
+
+
+			ENDCG
+
+		}
+
+		}
+
+	}
